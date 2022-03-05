2039156dc2b780ca564fc46e8a2843ae60e56437
ananya-singh7 <ananyasinghhh777@gmail.com>
2022-03-05 21:35:33 +0530

    Initial commit

---
+++ lib/data.dart
@@ -0,0 +1,58 @@
+Map<String, String> choice1 = {
+  "": "Make Correct Choices",
+
+  "SpicyRiceMeat": "Chicken Biryani",
+  "SpicyRiceEggs": "Egg Fried Rice",
+  "SpicyRicePaneer": "Paneer Pulao",
+  "SpicyRiceVeggies": "Veg Manchurian Fried Rice",
+
+  "SpicyPastaMeat": "Creamy Garlic Chicken Pasta",
+  "SpicyPastaEggs": "Egg Spaghetti",
+  "SpicyPastaPaneer": "Paneer Macaroni",
+  "SpicyPastaVeggies": "Veg Arrabiata Pasta",
+
+  "SpicyNaanMeat": "Butter Chicken with Tandoori Roti",
+  "SpicyNaanEggs": "Egg Masala Curry",
+  "SpicyNaanPaneer": "Paneer Tikka Masala with Butter Naan",
+  "SpicyNaanVeggies": "Hyderabadi Veg Korma with ",
+
+  "SpicyBreadMeat": "Grilled Chicken 'N' Cheese Sandwich",
+  "SpicyBreadEggs": "Avocado Egg Sandwich",
+  "SpicyBreadPaneer": "Smoked Tandoori Paneer Sandwich",
+  "SpicyBreadVeggies": "Bombay Grilled Cheese Sandwich",
+
+  "SweetBreadEggs": "Honey Berry Pancakes",
+
+  "SourRiceMeat": "Sweet 'N' Sour Chicken with Rice"
+
+};
+
+
+
+Map<String, String> choice2 = {
+  "": "Make Correct Choices",
+
+  "SpicyRiceMeat": "Chicken Biryani",
+  "SpicyRiceEggs": "Egg Fried Rice",
+  "SpicyRicePaneer": "Paneer Pulao",
+  "SpicyRiceVeggies": "Veg Manchurian Fried Rice",
+
+  "SpicyPastaMeat": "images/alfredo.jpg",
+  "SpicyPastaEggs": "Egg Spaghetti",
+  "SpicyPastaPaneer": "Paneer Macaroni",
+  "SpicyPastaVeggies": "images/arrabiata.jpg",
+
+  "SpicyNaanMeat": "Butter Chicken with Tandoori Roti",
+  "SpicyNaanEggs": "Egg Masala Curry",
+  "SpicyNaanPaneer": "Paneer Tikka Masala with Butter Naan",
+  "SpicyNaanVeggies": "Hyderabadi Veg Korma with ",
+
+  "SpicyBreadMeat": "Grilled Chicken 'N' Cheese Sandwich",
+  "SpicyBreadEggs": "images/avocado.jpg",
+  "SpicyBreadPaneer": "Smoked Tandoori Paneer Sandwich",
+  "SpicyBreadVeggies": "images/bombay.jpg",
+
+  "SweetBreadEggs": "images/pancakes1.jpg",
+  "SourRiceMeat": "images/sourchick.jpg"
+
+};
