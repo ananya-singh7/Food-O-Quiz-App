12ccc6d71d9f0ba282dac751d31682491c637f39
ananya-singh7 <89238834+ananya-singh7@users.noreply.github.com>
2022-03-09 20:54:23 +0530

    Update data.dart

--- lib/data.dart
+++ lib/data.dart
@@ -56,3 +56,92 @@ Map<String, String> choice2 = {
   "SourRiceMeat": "images/sourchick.jpg"
 
 };
+
+
+
+Map<String, String> choice3 = {
+  "": "Make Correct Choices",
+
+  "SpicyRiceMeat": "Chicken Biryani",
+  "SpicyRiceEggs": "Egg Fried Rice",
+  "SpicyRicePaneer": "Paneer Pulao",
+  "SpicyRiceVeggies": "Veg Manchurian Fried Rice",
+
+  "SpicyPastaMeat": "images/alfredo.jpg",
+  "SpicyPastaEggs": "Egg Spaghetti",
+  "SpicyPastaPaneer": "Paneer Macaroni",
+  "SpicyPastaVeggies": "Arrabiata Pasta is a popular Italian recipe that features a sauce made with spicy red chili peppers and tomatoes. In fact, the word “arrabiata” means angry in Italian, which refers to the bold spiciness of the dish .So if you’re looking to add a bit of heat to your next pasta night, this recipe is exactly what you need.",
+
+  "SpicyNaanMeat": "Butter Chicken with Tandoori Roti",
+  "SpicyNaanEggs": "Egg Masala Curry",
+  "SpicyNaanPaneer": "Paneer Tikka Masala with Butter Naan",
+  "SpicyNaanVeggies": "Hyderabadi Veg Korma with ",
+
+  "SpicyBreadMeat": "Grilled Chicken 'N' Cheese Sandwich",
+  "SpicyBreadEggs": "Restaurant-style breakfast sandwich at home! It's quick, easy and delicious! The sliced avocado is what makes this sandwich pop and gives it a unique flavor. It is chock-full of protein, as well, so this sandwich will keep you full until lunchtime.",
+  "SpicyBreadPaneer": "Smoked Tandoori Paneer Sandwich",
+  "SpicyBreadVeggies": "images/bombay.jpg",
+
+  "SweetBreadEggs": "Who doesn't love some good old fashioned pancakes for any meal of the day and that too with a drizzle of honey (kidding, probably half a bottle of honey if you are sane) and finished to perfection with handpicked berries. \n\n Pancakes are light and fluffy and only need a few simple ingredients, that you probably have in your kitchen right now so go and enjoy a sweet delicious meal and make sure to save some for next day to enjoy too, still stacked together with delicacy!",
+  "SourRiceMeat": "What ingredients could possibly give Sweet 'N' Sour chicken that tangy, sweet and well sour flavour? \n Actually quite a bizarre combination: ketchup, malt vinegar, brown sugar and pineapple. And of course the chicken chopped into bite-sized pieces, dredged in a flavourful coating then fried to crisp perfection, before being coated in that sweet and sour sauce. Indeed a true work of art!"
+
+};
+
+
+
+Map<String, String> choice4 = {
+  "": "Make Correct Choices",
+
+  "SpicyRiceMeat": "Chicken Biryani",
+  "SpicyRiceEggs": "Egg Fried Rice",
+  "SpicyRicePaneer": "Paneer Pulao",
+  "SpicyRiceVeggies": "Veg Manchurian Fried Rice",
+
+  "SpicyPastaMeat": "images/alfredo.jpg",
+  "SpicyPastaEggs": "Egg Spaghetti",
+  "SpicyPastaPaneer": "Paneer Macaroni",
+  "SpicyPastaVeggies": "images/arrabiata.jpg",
+
+  "SpicyNaanMeat": "Butter Chicken with Tandoori Roti",
+  "SpicyNaanEggs": "Egg Masala Curry",
+  "SpicyNaanPaneer": "Paneer Tikka Masala with Butter Naan",
+  "SpicyNaanVeggies": "Hyderabadi Veg Korma with ",
+
+  "SpicyBreadMeat": "Grilled Chicken 'N' Cheese Sandwich",
+  "SpicyBreadEggs": "images/avocado.jpg",
+  "SpicyBreadPaneer": "Smoked Tandoori Paneer Sandwich",
+  "SpicyBreadVeggies": "images/bombay.jpg",
+
+  "SweetBreadEggs": "3 cups white whole wheat flour (or all-purpose flour) \n 3/4 cups whole wheat flour \n 1/4 cup sugar \n 2 tablespoons plus 1 ¼ teaspoons baking powder \n 1 and a 1/2 teaspoon salt \n 2 bags (16 ounces each) frozen mixed berries, about 8 cups total \n 1 tablespoon balsamic vinegar \n 2 to 4 tablespoons honey, plus more for serving \n 1 stick (½ cup) unsalted butter, plus more for cooking \n 1/2 cup vegetable oil such as expeller-pressed canola or sunflower oil, plus more for cooking \n 6 large eggs \n 2 and a 1/2 cups skim milk \n 1 cup nonfat plain Greek yogurt, plus more for serving",
+  "SourRiceMeat": "CHICKEN \n\n 1 lb boneless, skinless chicken breast (455 g) \n salt to taste \n pepper to taste \n ½ cup cornstarch (60 g) \n 2 eggs (beaten) \n oil to fry \n\n SAUCE \n\n 1 tablespoon oil \n ½ tablespoon garlic, minced \n 1 bell pepper, diced \n ¾ cup cider vinegar (180 mL) \n 1 tablespoon soy sauce \n ¼ cup ketchup (55 g) \n ½ cup sugar (100 g) \n\n GARNISH \n\n scallion, chopped \n sesame seeds"
+
+};
+
+
+Map<String, String> choice5 = {
+  "": "Make Correct Choices",
+
+  "SpicyRiceMeat": "Chicken Biryani",
+  "SpicyRiceEggs": "Egg Fried Rice",
+  "SpicyRicePaneer": "Paneer Pulao",
+  "SpicyRiceVeggies": "Veg Manchurian Fried Rice",
+
+  "SpicyPastaMeat": "images/alfredo.jpg",
+  "SpicyPastaEggs": "Egg Spaghetti",
+  "SpicyPastaPaneer": "Paneer Macaroni",
+  "SpicyPastaVeggies": "images/arrabiata.jpg",
+
+  "SpicyNaanMeat": "Butter Chicken with Tandoori Roti",
+  "SpicyNaanEggs": "Egg Masala Curry",
+  "SpicyNaanPaneer": "Paneer Tikka Masala with Butter Naan",
+  "SpicyNaanVeggies": "Hyderabadi Veg Korma with ",
+
+  "SpicyBreadMeat": "Grilled Chicken 'N' Cheese Sandwich",
+  "SpicyBreadEggs": "images/avocado.jpg",
+  "SpicyBreadPaneer": "Smoked Tandoori Paneer Sandwich",
+  "SpicyBreadVeggies": "images/bombay.jpg",
+
+  "SweetBreadEggs": "1. Mix flours, sugar, baking powder and salt in a large container. Store covered, up to a couple of weeks. \n\n 2. For the honey berry topping, put 1 bag of the mixed berries in a medium microwave-safe bowl. \n\n 3. Microwave on high, stirring a couple of times, until berries are softened but still cold, about 2 minutes. \n\n 4. Stir in the vinegar and 2 tablespoons of the honey. Add more honey to taste. \n\n 5. Refrigerate, covered, up to 2 days. Serve at room temperature. \n\n 6. When you are ready to cook, heat oven to 200 degrees; place a baking sheet with a wire rack set on it in the oven. \n\n 7. Let the remaining bag of mixed berries sit on the counter to soften slightly. \n\n 8. Put the 1 stick butter into a microwave-safe bowl; cover loosely with wax paper. \n\n 9. Microwave on high (100 percent power) until melted, about 45 seconds. \n\n 10. Stir in the ½ cup oil. \n\n 11. Whisk eggs smooth in a large bowl. \n\n 12. Whisk in skim milk and the 1 cup yogurt until smooth. \n\n 13. Whisk in butter-oil mixture. \n\n 14. Add dry ingredients; whisk gently just until all dry ingredients are moistened. Do not overmix, or your pancakes will be tough. \n\n 15. Gently fold in the contents of the remaining bag of mixed berries. \n\n 16. Heat 1 or 2 large nonstick skillet(s) or a nonstick griddle over medium heat until a drop of the pancake batter bubbles furiously when dropped on the surface. \n\n 17. Lightly oil and butter the cooking surface. \n\n 18. Spoon out about ¼ cup of batter per pancake.\n\n 19. Spread the batter with the back of the spoon, so it is thinned out a little. \n\n 20. Cook until a few bubbles break on top and the bottom is golden, about 2 minutes. \n\n 21. Gently flip pancake over; cook until second side is golden, 1 to 2 minutes more. \n\n 22. Keep oiling the cooking surface and adjusting the heat as you go along, so pancakes are golden and not overly browned. \n\n 23. Transfer cooked pancakes to the wire rack on the baking sheet in the oven. \n\n 24. Keep warm until enough are cooked to serve. \n\n 25. Serve pancakes with the honey berry topping and more yogurt.",
+  "SourRiceMeat": "1. Season chicken with salt and pepper. \n\n 2. Coat with cornstarch, dip in beaten egg. \n\n 3. Fry until golden, crispy and cooked through. Set aside. Dab with paper towel if greasy. \n\n 4. In a large pan, heat oil and fry garlic until fragrant; add the bell pepper, cider vinegar, soy sauce, ketchup and sugar. \n\n 5. Bring it to a boil. \n\n 6. When the sauce has thickened, add the fried chicken. \n\n 7. Toss to coat the chicken with the sauce. \n\n 8. Garnish with chopped scallions and sesame seeds and serve it with rice."
+
+};
